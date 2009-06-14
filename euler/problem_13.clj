5200a5b15f5addd21bac00899f96d51273fae3db
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2009-06-14 19:06:51 -0300

    Solving problem #13.

---
+++ euler/problem_13.clj
@@ -0,0 +1,12 @@
+(ns euler.problem-13
+  (:use [clojure.contrib.str-utils :only (re-split)]))
+
+(defn load-input [f]
+  "Loads the problem input from the file f."
+  (map bigint (re-split #"\n" (slurp f))))
+
+(defn solve [f]
+  "Returns the first ten digits of the sum of the following one-hundred 50-digit numbers."
+  (apply str (take 10 (str (reduce + (load-input f))))))
+
+(println (solve "euler/input/problem_13.txt"))
