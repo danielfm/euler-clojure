d70b500dfbee43cf629b119ddf656d5ae8b2bcbb
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2009-06-14 19:48:25 -0300

    Removed blank line.

--- euler/problem_16.clj
+++ euler/problem_16.clj
@@ -2,7 +2,6 @@
   (:use [clojure.contrib.math :only (expt)]
 	[euler.util :only (to-int)]))
 
-
 (defn solve []
   "Returns the sum of the digits of the number 2^1000."
   (reduce + (map to-int (str (expt 2 1000)))))
