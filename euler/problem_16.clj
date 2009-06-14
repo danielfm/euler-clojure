b672ca95e6d6565754ddd54954c4ef6611dc1aaa
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2009-06-14 19:48:01 -0300

    Solved problem #16.

---
+++ euler/problem_16.clj
@@ -0,0 +1,10 @@
+(ns euler.problem-16
+  (:use [clojure.contrib.math :only (expt)]
+	[euler.util :only (to-int)]))
+
+
+(defn solve []
+  "Returns the sum of the digits of the number 2^1000."
+  (reduce + (map to-int (str (expt 2 1000)))))
+
+(println (solve))
