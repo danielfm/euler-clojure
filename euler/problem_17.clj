560ced63a204879f51edb58543b00cb4b8257dfa
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2009-06-16 21:10:59 -0300

    Solved problem #17

---
+++ euler/problem_17.clj
@@ -0,0 +1,30 @@
+(ns euler.problem-17
+  (:use [clojure.contrib.def :only (defvar)]))
+
+(defvar one-to-nineteen
+  ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"
+   "eleven" "twelve" "thirteen" "fourteen" "fifteen" "sixteen" "seventeen"
+   "eighteen" "nineteen"]
+  "Cardinal numbers between 1 and 19.")
+
+(defvar twenty-to-ninety
+  ["twenty" "thirty" "forty" "fifty" "sixty" "seventy" "eighty" "ninety"]
+  "Tens.")
+
+(defn n-in-words [n]
+  "Returns a description in words of the natural number n (between 1 and 1000)."
+  (cond
+    (< n 20) (if (zero? n) ""
+		 (nth one-to-nineteen (- n 1)))
+    (< n 100) (str (nth twenty-to-ninety (- (quot n 10) 2))
+		   (n-in-words (rem n 10)))
+    (< n 1000) (str (n-in-words (quot n 100)) "hundred"
+		    (if (zero? (rem n 100)) ""
+			(str "and" (n-in-words (rem n 100)))))
+    (= n 1000) "onethousand"))
+
+(defn solve []
+  "Returns the number of letters used to write in words all the numbers from 1 to 1000."
+  (reduce + (map count (map n-in-words (range 1 1001)))))
+
+(println (solve))
