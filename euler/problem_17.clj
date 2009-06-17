bc207edb8091324c09659a9f7e0443b9732ee58a
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2009-06-16 21:18:22 -0300

    Small improvement on Problem #17.

--- euler/problem_17.clj
+++ euler/problem_17.clj
@@ -2,7 +2,7 @@
   (:use [clojure.contrib.def :only (defvar)]))
 
 (defvar one-to-nineteen
-  ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"
+  ["" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"
    "eleven" "twelve" "thirteen" "fourteen" "fifteen" "sixteen" "seventeen"
    "eighteen" "nineteen"]
   "Cardinal numbers between 1 and 19.")
@@ -14,14 +14,13 @@
 (defn n-in-words [n]
   "Returns a description in words of the natural number n (between 1 and 1000)."
   (cond
-    (< n 20) (if (zero? n) ""
-		 (nth one-to-nineteen (- n 1)))
-    (< n 100) (str (nth twenty-to-ninety (- (quot n 10) 2))
-		   (n-in-words (rem n 10)))
-    (< n 1000) (str (n-in-words (quot n 100)) "hundred"
-		    (if (zero? (rem n 100)) ""
-			(str "and" (n-in-words (rem n 100)))))
-    (= n 1000) "onethousand"))
+   (< n 20) (nth one-to-nineteen n)
+   (< n 100) (str (nth twenty-to-ninety (- (quot n 10) 2))
+		  (n-in-words (rem n 10)))
+   (< n 1000) (str (n-in-words (quot n 100)) "hundred"
+		   (if (zero? (rem n 100)) ""
+		       (str "and" (n-in-words (rem n 100)))))
+   (= n 1000) "onethousand"))
 
 (defn solve []
   "Returns the number of letters used to write in words all the numbers from 1 to 1000."
