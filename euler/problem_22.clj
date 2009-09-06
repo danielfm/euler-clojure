80c67c4909f3445f90796f63d4d41c99c8a815bc
danielfm <daniel.tritone@gmail.com>
2009-09-06 17:19:15 -0300

    Renamed method.

--- euler/problem_22.clj
+++ euler/problem_22.clj
@@ -4,12 +4,12 @@
   "Loads the problem input from the file f."
   (map vector (iterate inc 1) (sort (re-seq #"\w+" (slurp f)))))
 
-(defn rank [[idx name]]
+(defn score [[idx name]]
   "Returns the score for the given entry e."
   (* idx (apply + (map #(- (int %) 64) name))))
 
 (defn solve [f]
   "Finds the total of all the name scores in the file f."
-  (reduce + (map rank (load-input f))))
+  (reduce + (map score (load-input f))))
 
 (println (solve "euler/input/problem_22.txt"))
