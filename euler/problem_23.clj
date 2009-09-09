ce029ebefcc78ea5a32d0d52d432acc84de754ad
danielfm <daniel.tritone@gmail.com>
2009-09-08 23:44:59 -0300

    Fixing typo.

--- euler/problem_23.clj
+++ euler/problem_23.clj
@@ -5,14 +5,14 @@
   "Returns whether n is abundant."
   (> (sum-proper-divisors n) n))
 
-(defn sum-of-abundants? [elegible n]
+(defn sum-of-abundants? [eligible n]
   "Returns whether n can be written as the sum of two abundant numbers."
-  (some #(contains? elegible (- n %)) elegible))
+  (some #(contains? eligible (- n %)) eligible))
 
 (defn solve []
   "Returns the sum of all the positive integers which cannot be written as the sum of two abundant numbers."
-  (let [rng (range 1 28123) elegible (into #{} (filter abundant? rng))]
-    (reduce + (filter (partial (complement sum-of-abundants?) elegible) rng))))
+  (let [rng (range 1 28123) eligible (into #{} (filter abundant? rng))]
+    (reduce + (filter (partial (complement sum-of-abundants?) eligible) rng))))
 
 ; Expected result: 4179871
 (println (solve))
