67e63008c42ed433879bc03996ffce66f776ec1e
danielfm <daniel.tritone@gmail.com>
2009-09-08 21:40:13 -0300

    Solved problem #23.

---
+++ euler/problem_23.clj
@@ -0,0 +1,19 @@
+(ns euler.problem-23
+  (:use [euler.util :only (sum-proper-divisors)]
+	[clojure.contrib.def :only (defvar)]))
+
+(defn abundant? [n]
+  "Returns whether n is abundant."
+  (> (sum-proper-divisors n) n))
+
+(defn sum-of-abundants? [elegible n]
+  "Returns whether n can be written as the sum of two abundant numbers."
+  (some #(contains? elegible (- n %)) elegible))
+
+(defn solve []
+  "Returns the sum of all the positive integers which cannot be written as the sum of two abundant numbers."
+  (let [rng (range 1 28123) elegible (into #{} (filter abundant? rng))]
+    (reduce + (filter (complement (partial sum-of-abundants? elegible)) rng))))
+
+; Expected result: 4179871
+(println (solve))
