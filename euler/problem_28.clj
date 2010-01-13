bdef0de4060344ed1433618388cd7fb99ef841ae
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2010-01-13 21:44:48 -0200

    Solved #28.

---
+++ euler/problem_28.clj
@@ -0,0 +1,13 @@
+(ns euler.problem-28)
+
+(defn sum-diagonals [n]
+  "Sums the diagonals of the a nxn espiral."
+  (if (= n 1) n
+      (reduce + (take 4 (iterate #(- % (dec n)) (* n n))))))
+
+(defn solve []
+  "Returns the sum of the numbers on the diagonals in a 1001x1001 spiral."
+  (reduce + (map sum-diagonals (range 1 1002 2))))
+
+; Expected result: 669171001
+(time (println (solve)))
